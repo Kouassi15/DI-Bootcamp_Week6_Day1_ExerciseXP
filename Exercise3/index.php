298519191d26a9f45b1404ca3bb9279a7a9045f1
Kouassi15 <rachelleservices8@gmail.com>
2023-01-31 13:37:31 +0100

    first commit

---
+++ Exercise3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Two PHP Variables Using Single Echo
+      Instructions</title>
+</head>
+<body>
+    <?php
+    $message1 = "Good Morning. ";
+    $message2 = "Have a nice day!";
+     echo $message1,$message2;
+    ?>
+</body>
+</html>
